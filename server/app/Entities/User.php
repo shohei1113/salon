ea40a86dff3ccb9299d12a845c81a75d8e6d675e
shohei1113 <kanatani1113@gmail.com>
2019-05-12 13:12:02 +0900

    Exec command: php artisan make:model Entites/User

--- server/app/Entities/User.php
+++ server/app/Entities/User.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Entities;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
