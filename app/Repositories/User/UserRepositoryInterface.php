289feca7759d172b39f19ef1d2ff6b2ae83c64a9
shohei1113 <kanatani1113@gmail.com>
2019-06-09 14:05:24 +0900

    認証機能のリポジトリ作成

--- app/Repositories/User/UserRepositoryInterface.php
+++ app/Repositories/User/UserRepositoryInterface.php
@@ -1,6 +1,13 @@
-<?phpn
+<?php
 namespace App\Repositories\User;
 
 interface UserRepositoryInterface
 {
+    public function createUser($attribute);
+
+    public function updateUser($attribute);
+
+    public function fetchUserById($id);
+
+    public function fetchUserByToken($token);
 }
--- resources/views/auth/email/pre_register.blade.php
+++ resources/views/auth/email/pre_register.blade.php
@@ -1,4 +1,12 @@
-サイトへのアカウント仮登録が完了しました。<br>
-<br>
-以下のURLからログインして、本登録を完了させてください。<br>
-{{url('http://stg.hayaokuri.com/register/?token='.$user->email_verify_token)}}
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <link rel="stylesheet" href="{{{asset('/css/mail.css')}}}">
+    </head>
+    <body>
+    サイトへのアカウント仮登録が完了しました。<br>
+    <br>
+    以下のURLからログインして、本登録を完了させてください。<br>
+    {{url('http://stg.hayaokuri.com/register?token='.$user->email_verify_token)}}
+    </body>
+</html>
