9153964a4bf1c6e9420b8bcb9a2eca39072166e9
shohei1113 <kanatani1113@gmail.com>
2019-06-13 01:35:27 +0900

    Imageのリポジトリ作成

--- app/Repositories/Image/ImageRepositoryInterface.php
+++ app/Repositories/Image/ImageRepositoryInterface.php
@@ -1,6 +1,18 @@
 <?php
+
 namespace App\Repositories\Image;
 
+/**
+ * Interface ImageRepositoryInterface
+ * @package App\Repositories\Image
+ */
 interface ImageRepositoryInterface
 {
+    /**
+     * @param $user
+     * @param $path
+     * @param $type
+     * @return mixed
+     */
+    public function updateImage($user, $path, $type);
 }
