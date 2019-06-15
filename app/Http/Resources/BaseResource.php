727c0f77500fd309ffd74be0cf96e5cbd4d14b11
shohei1113 <kanatani1113@gmail.com>
2019-06-16 08:51:42 +0900

    Exec command: php artisan make:resource BaseResource

--- app/Http/Resources/BaseResource.php
+++ app/Http/Resources/BaseResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class SalonAllResource extends JsonResource
+class BaseResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
