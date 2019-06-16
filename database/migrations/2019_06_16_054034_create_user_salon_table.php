c0f08cb4fc9db249c3c5231b25697ad25581c15b
shohei1113 <kanatani1113@gmail.com>
2019-06-16 21:24:13 +0900

    サロン購入管理用の中間テーブル作成

--- database/migrations/2019_06_16_054034_create_user_salon_table.php
+++ database/migrations/2019_06_16_054034_create_user_salon_table.php
@@ -14,8 +14,17 @@ class CreateUserSalonTable extends Migration
     public function up()
     {
         Schema::create('user_salon', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('salon_id')->unsigned();
+            $table->primary(['user_id', 'salon_id']);
+
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+            $table->foreign('salon_id')
+                ->references('id')
+                ->on('salons');
         });
     }
 
